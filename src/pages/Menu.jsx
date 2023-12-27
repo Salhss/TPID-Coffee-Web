d40dd48e1ab60a5c05842d031838e393e339a830
OMO-git <omogit3@gmail.com>
2023-12-27 17:56:41 +0700

    refactor: change menu category name ui

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -6,9 +6,18 @@ import { Link, useLocation } from "react-router-dom";
 const token = localStorage.getItem("Authorization");
 
 export default function Menu() {
-    const [menuArr, setMenuArr] = useState([])
-    const location = useLocation()
-    console.log(location.hash)
+  const [menuArr, setMenuArr] = useState([]);
+  const [activeLink, setActiveLink] = useState(null);
+  const { hash } = useLocation();
+
+  const formattedHash = (str) => {
+    return str.toLowerCase().replace(" ", "-");
+  };
+
+  const handleLinkClick = (index) => {
+    setActiveLink(index);
+  };
+
   useEffect(() => {
     const menuData = async () => {
       try {
@@ -30,34 +39,47 @@ export default function Menu() {
       }
     };
 
-    menuData().then((data) => {
-      setMenuArr(() => data.categories)
-    }).catch(console.log)
+    menuData()
+      .then((data) => {
+        setMenuArr(() => data.categories);
+      })
+      .catch(console.log);
   }, []);
 
-  console.log( menuArr)
+  //   console.log( menuArr)
   return (
     <div className=" min-h-dvh font-ubuntu">
-        {menuArr.length !== 0 ? (
-      <div className=" pt-3">
-        <div className="w-full">
-          <h1 className=" text-lg text-gray-700 font-semibold uppercase text-center">
-            Menu
-          </h1>
-          <div className="w-full overflow-x-scroll ">
-          <div className="categories flex flex-row mt-3">
-               
-
-            {menuArr.map((item, index) => (
-                <div key={index} className="w-full px-3 py-2">
-                    <Link to={`/menu#${item.category_name.toLowerCase().replace(' ', '-')}`} className="text-sm font-medium capitalize text-nowrap">{item.category_name}</Link>
-                </div>
-            ))}
-          </div>
+      {menuArr.length !== 0 ? (
+        <div className=" pt-3">
+          <div className="w-full">
+            <h1 className=" text-lg text-gray-700 font-semibold uppercase text-center">
+              Menu
+            </h1>
+            <div className="w-full overflow-x-scroll ">
+              <div className="categories flex flex-row mt-3">
+                {menuArr.map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleLinkClick(index)}
+                    className={`w-full px-3 py-2 flex justify-center items-center ${
+                      activeLink === index ? "border-b-4 border-black" : ""
+                    }`}
+                  >
+                    <Link
+                      to={`/menu#${formattedHash(item.category_name)}`}
+                      className="text-sm font-medium capitalize text-center text-nowrap "
+                    >
+                      {item.category_name}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-        ): <p>Loading...</p>}
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
